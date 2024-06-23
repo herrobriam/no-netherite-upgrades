670c020aabf0874f2fd7eeee29095fcc4b759ff2
herrobriam <131239470+herrobriam@users.noreply.github.com>
2024-06-23 13:06:45 +0100

    Initial commit

---
+++ src/main/java/net/herrobriam/nonetheriteupgrades/NoNetheriteUpgrades.java
@@ -0,0 +1,16 @@
+package net.herrobriam.nonetheriteupgrades;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NoNetheriteUpgrades implements ModInitializer {
+	public static final String MOD_ID = "no-netherite-upgrades";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/herrobriam/nonetheriteupgrades/NoNetheriteUpgradesClient.java
@@ -0,0 +1,10 @@
+package net.herrobriam.nonetheriteupgrades;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoNetheriteUpgradesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
